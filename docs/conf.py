8bab766bd898e0cbb4afada72a07804f2372adae
Jason Liu <jasonxliu2010@gmail.com>
2019-07-15 11:01:25 -0400

    minor: tried to fix problem with readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,8 @@ release = simulus.__version__
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
